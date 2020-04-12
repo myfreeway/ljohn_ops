dba2b4393a49e4414f843b6db630c3d46ae2f705
jiangguifan <jiangguifan@phone580.com>
2020-04-12 21:22:21 +0800

    恢复ipv4转发

--- linux-init/centos7_init.sh
+++ linux-init/centos7_init.sh
@@ -79,7 +79,7 @@ EOF
 sysctl_config(){
     cp /etc/sysctl.conf /etc/sysctl.conf.bak
     cat > /etc/sysctl.conf << EOF
-    net.ipv4.ip_forward = 0
+    net.ipv4.ip_forward = 1
     net.ipv4.conf.default.rp_filter = 1
     net.ipv4.conf.default.accept_source_route = 0
     kernel.sysrq = 0
